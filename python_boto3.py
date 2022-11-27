16b1f25f38d2799748cfd2bf0303289b150c2eb3
Dani <dan.avila7@gmail.com>
2022-11-26 19:19:46 -0500

    fix objects.all

--- python_boto3.py
+++ python_boto3.py
@@ -1,17 +1,18 @@
 import boto3
-import logging
 
 
 s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
     bucketlist = []
-    for bucket in s3.buckets.all():
-        if bucket.name == "prueba-bucket-aws-cli":
-            for objeto_bucket in bucket.objects.all():
-                bucketlist.append(objeto_bucket.key)
+    bucket = s3.Bucket('prueba-bucket-aws-cli')
+    
+    for objeto_bucket in bucket.objects.all():
+        bucketlist.append(objeto_bucket.key)
       
     return {
         "statusCode": 200,
         "body": bucketlist
     }
+    
+
